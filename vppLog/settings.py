2b4ef90c1e0c45cf6149934614323433a813528b
kongderui <kongderui@baoyinxiaofei.com>
2018-05-14 16:35:48 +0800

    create

--- vppLog/settings.py
+++ vppLog/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'vppReve.urls'
+ROOT_URLCONF = 'vppLog.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'vppReve.wsgi.application'
+WSGI_APPLICATION = 'vppLog.wsgi.application'
 
 
 # Database
--- vppLog/wsgi.py
+++ vppLog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vppReve.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vppLog.settings")
 
 application = get_wsgi_application()
